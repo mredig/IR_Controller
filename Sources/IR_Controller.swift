887efb1f199127c342f0f16edba8099ab04d0902
Michael Redig <mredig@gmail.com>
2024-01-23 16:52:54 -0600

    should blink

--- Sources/IR_Controller.swift
+++ Sources/IR_Controller.swift
@@ -1,8 +1,25 @@
 import ArgumentParser
+import SwiftyGPIO
+#if os(Linux)
+import Glibc
+#else
+import Darwin
+#endif
+
+let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi4)
 
 @main
 struct IR_Controller: AsyncParsableCommand {
+
 	mutating func run() async throws {
+		gpios[.P4]?.direction = .OUT
 		print("Hello, world!")
+
+		while true {
+			gpios[.P4]?.value = 1
+			sleep(1)
+			gpios[.P4]?.value = 0
+			sleep(1)
+		}
 	}
 }
