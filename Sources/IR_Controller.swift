6a84d3d1775c978ab6fc33ae7430c25821b0b6ec
Michael Redig <mredig@gmail.com>
2024-01-23 18:29:20 -0600

    pwm test 4

--- Sources/IR_Controller.swift
+++ Sources/IR_Controller.swift
@@ -24,7 +24,9 @@ struct IR_Controller: AsyncParsableCommand {
 //			pwm?.sendDataWithPattern(values: data)
 //			pwm?.waitOnSendData()
 //			pwm?.cleanupPattern()
+			print("sending")
 			signal(pwm: pwm)
+			print("sent")
 			sleep(1)
 		}
 
@@ -40,29 +42,63 @@ struct IR_Controller: AsyncParsableCommand {
 	}
 
 	func signal(pwm: PWMOutput) {
-//		let data: [UInt32] = [
-//			180,  80,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10
-//		].map { $0 * 1000 }
-//		var isOn = false
-//
-//		for period in data {
-//			if isOn {
-//				isOn = false
-//				pwm.stopPWM()
-//				usleep(period)
-//			} else {
-//				isOn = true
-//				pwm.startPWM(period: 26316, duty: 50)
-//				usleep(period)
-//			}
-//		}
-		print("Starting")
-		pwm.startPWM(period: 26316, duty: 50)
-		sleep(2)
-		print("Stopping")
+		let data: [UInt32] = [
+			180,  80,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10,  30,  10
+		].map { $0 * 1000 }
+
+		var ts = timespec()
+
+		var isOn = false
+
+		var passage: [Int] = []
+		for period in data {
+			defer {
+				clock_gettime(CLOCK_MONOTONIC_RAW, &ts)
+				passage.append(ts.tv_nsec)
+			}
+
+			if isOn {
+				isOn = false
+				pwm.stopPWM()
+				delay(period)
+			} else {
+				isOn = true
+				pwm.startPWM(period: 26316, duty: 50)
+				delay(period)
+			}
+		}
 		pwm.stopPWM()
-		sleep(2)
-		pwm.waitOnSendData()
+//		print("Starting")
+//		pwm.startPWM(period: 26316, duty: 50)
+//		sleep(2)
+//		print("Stopping")
+//		pwm.stopPWM()
+//		sleep(2)
+//		pwm.waitOnSendData()
+		var previousTime = 0
+		for time in passage {
+			let elapsed = time - previousTime
+//			print("\(elapsed) ns")
+			print("\(elapsed / 1000) microseconds")
+			previousTime = time
+		}
+		let expected = data.reduce(0, +)
+		print("\((passage.last! - passage.first!) / 1000) total, \(expected / 1000) expected")
+	}
+
+	@inline(__always)
+	private func delay(_ nanoseconds: UInt32) {
+		var ts = timespec()
+		
+		clock_gettime(CLOCK_MONOTONIC_RAW, &ts)
+		let startTime = ts.tv_nsec
+
+		var passedTime = 0
+
+		while passedTime < nanoseconds {
+			clock_gettime(CLOCK_MONOTONIC_RAW, &ts)
+			passedTime = ts.tv_nsec - startTime
+		}
 	}
 
 	enum IRError: Error {
